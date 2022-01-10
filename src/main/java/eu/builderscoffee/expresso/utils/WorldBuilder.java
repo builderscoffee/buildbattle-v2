694339d1d0ca6ead486d72830222336e65fde81d
GameurDuXVI <gameur.codeur@gmail.com>
2022-01-10 08:50:41 +0100

    Fix WorldBuilder to the new PlotSquared version

--- src/main/java/eu/builderscoffee/expresso/utils/WorldBuilder.java
+++ src/main/java/eu/builderscoffee/expresso/utils/WorldBuilder.java
@@ -1,188 +1,160 @@
 package eu.builderscoffee.expresso.utils;
 
 import com.plotsquared.bukkit.util.BukkitSetupUtils;
+import com.plotsquared.core.PlotSquared;
 import com.plotsquared.core.configuration.ConfigurationNode;
+import com.plotsquared.core.plot.PlotAreaType;
+import com.plotsquared.core.setup.CommonSetupSteps;
+import com.plotsquared.core.setup.PlotAreaBuilder;
+import com.plotsquared.core.setup.SettingsNodesWrapper;
 import com.plotsquared.core.util.SetupUtils;
-import lombok.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.SneakyThrows;
 import lombok.experimental.Accessors;
 import org.bukkit.Material;
-import org.bukkit.inventory.ItemStack;
+import org.bukkit.event.Listener;
 
-public class WorldBuilder {
+import java.lang.reflect.Method;
 
-    @Getter(AccessLevel.PROTECTED)
-    private final SetupObject setupObj;
-    @Getter(AccessLevel.PROTECTED)
-    private final String generator;
-    @Setter
-    @Getter
-    private int terrain = 0;
+@Getter
+@Setter
+public class WorldBuilder implements Listener {
 
-    public WorldBuilder(String generator, WorldTypes worldType) {
-        this.generator = generator;
+    private final PlotAreaBuilder builder;
 
+    @SneakyThrows
+    public WorldBuilder(String generator, WorldType worldType) {
         // Update generators
-        BukkitSetupUtils.manager.updateGenerators();
-
-        // Create new setup object
-        this.setupObj = new SetupObject();
+        PlotSquared.platform().setupUtils().updateGenerators(true);
 
         // if generator not exist, throw error
         if (!BukkitSetupUtils.generators.containsKey(generator)) {
-            throw new RuntimeException(generator + " generator doesn't exist. Choose between: " + String.join(", ", BukkitSetupUtils.generators.keySet()));
+            throw new RuntimeException("Generator \"" + generator + "\" doesn't exist. Choose between: " + String.join(", ", BukkitSetupUtils.generators.keySet()));
         }
 
-        // Store generators & world type
-        getSetupObj().setupGenerator = generator;
-        getSetupObj().plotManager = generator;
-        getSetupObj().type = worldType.id;
+        // Create the PlotAreaBuilder
+        builder = PlotAreaBuilder.newBuilder();
 
-        // Store configurations depends on generator
-        getSetupObj().step = SetupUtils.generators.get(setupObj.plotManager).getPlotGenerator()
-                .getNewPlotArea("CheckingPlotSquaredGenerator", null, null, null).getSettingNodes();
-    }
+        // Setting generator
+        builder.generatorName("PlotSquared");
 
-    public void generate(String world) {
-        // Store world name & terrain
-        getSetupObj().world = world;
-        getSetupObj().terrain = terrain;
+        // Setting plot area type
+        builder.plotAreaType(PlotAreaType.NORMAL);
 
-        // Start generating world
-        BukkitSetupUtils.manager.setupWorld(getSetupObj());
-    }
-
-    public enum WorldTypes {
-        DEFAULT(0),
-        AUGMENTED(1),
-        PARTIAL(2);
-
-        public int id;
-
-        WorldTypes(int id) {
-            this.id = id;
+        // Create node wrapper if not exist
+        if (builder.settingsNodesWrapper() == null) {
+            builder.plotManager(builder.generatorName());
+            Method method = CommonSetupSteps.class.getDeclaredMethod("wrap", String.class);
+            method.setAccessible(true);
+            builder.settingsNodesWrapper((SettingsNodesWrapper) method.invoke(null, builder.plotManager()));
+            SetupUtils.generators.get(builder.plotManager()).getPlotGenerator()
+                    .processAreaSetup(builder);
         }
     }
 
+    /**
+     * Function to simplify the configuration of a setting node
+     * @param key Key
+     * @param value Value
+     * @return Return true if it has been changed
+     */
+    public boolean configureNode(String key, String value) {
+        for (ConfigurationNode settingsNode : builder.settingsNodesWrapper().getSettingsNodes()) {
+            if (settingsNode.getConstant().equals(key)) {
+                settingsNode.setValue(value);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Function to get a setting node
+     * @param key Key
+     * @return Return the configuration node
+     */
+    public ConfigurationNode getNode(String key) {
+        for (ConfigurationNode settingsNode : builder.settingsNodesWrapper().getSettingsNodes()) {
+            if (settingsNode.getConstant().equals(key)) {
+                return settingsNode;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Check if a setting node exist
+     * @param key Key
+     * @return Return true if exist
+     */
+    public boolean hasNode(String key) {
+        return getNode(key) != null;
+    }
+
+    /**
+     * Get all setting nodes of a generator
+     * @return
+     */
+    public ConfigurationNode[] getNodes() {
+        return builder.settingsNodesWrapper().getSettingsNodes();
+    }
+
+    /**
+     * Setup the world
+     * @param worldName The world name
+     */
+    public void setup(String worldName) {
+        builder.worldName(worldName);
+        PlotSquared.platform().setupUtils().setupWorld(builder);
+    }
+
+    public enum WorldType {
+        NORMAL,
+        AUGMENTED,
+        PARTIAL
+    }
+
+
+    @Accessors(chain = true)
     @Getter
     @Setter
-    @Accessors(chain = true)
     public static class DefaultWorldBuilder extends WorldBuilder {
-        private int plotHeight;
-        private int plotSize;
-        @NonNull
-        private ItemStack plotFilling = new ItemStack(Material.STONE);
-        @NonNull
-        private ItemStack plotFloor = new ItemStack(Material.STONE);
-        @NonNull
-        private ItemStack wall = new ItemStack(Material.STONE);
-        @NonNull
-        private ItemStack wallClaimed = new ItemStack(Material.STONE);
-        private int roadWidth;
-        private int roadHeight;
-        @NonNull
-        private ItemStack roadBlock = new ItemStack(Material.STONE);
-        @NonNull
-        private ItemStack wallFilling = new ItemStack(Material.STONE);
-        private int wallHeight;
-        private boolean bedrock;
+        private int plotHeight = 62;
+        private int plotWidth = 42;
+        private Material plotBlock = Material.STONE;
+        private boolean blockOnTopOfPlotBlock = true;
+        private Material plotFloorBlock = Material.GRASS_BLOCK;
+        private Material topWallBlockUnClaimed = Material.STONE_SLAB;
+        private Material topWallBlockClaimed = Material.SANDSTONE_SLAB;
+        private int roadWidth = 7;
+        private int roadHeight = 62;
+        private Material roadBlock = Material.QUARTZ_BLOCK;
+        private Material wallFillingBlock = Material.STONE;
+        private int wallHeight = 62;
+        private boolean hasBedrock = true;
 
         public DefaultWorldBuilder() {
-            super("PlotSquared", WorldBuilder.WorldTypes.DEFAULT);
-
-            // Get default values from default generator
-            for (ConfigurationNode node : getSetupObj().step) {
-                switch (node.getConstant()) {
-                    case "plot.height":
-                        plotHeight = (int) node.getDefaultValue();
-                        break;
-                    case "plot.size":
-                        plotSize = (int) node.getDefaultValue();
-                        break;
-                    case "plot.filling":
-                        val split0 = ((String) node.getDefaultValue()).split(":");
-                        plotFilling = new ItemStack(Integer.valueOf(split0[0]), 1, Short.valueOf(split0[1]));
-                        break;
-                    case "plot.floor":
-                        val split1 = ((String) node.getDefaultValue()).split(":");
-                        plotFloor = new ItemStack(Integer.valueOf(split1[0]), 1, Short.valueOf(split1[1]));
-                        break;
-                    case "wall.block":
-                        val pb0 = ((PlotBlock) node.getDefaultValue());
-                        wall = new ItemStack(pb0.id, 1, pb0.data);
-                        break;
-                    case "wall.block_claimed":
-                        val pb1 = ((PlotBlock) node.getDefaultValue());
-                        wallClaimed = new ItemStack(pb1.id, 1, pb1.data);
-                        break;
-                    case "road.width":
-                        roadWidth = (int) node.getDefaultValue();
-                        break;
-                    case "road.height":
-                        roadHeight = (int) node.getDefaultValue();
-                        break;
-                    case "road.block":
-                        val pb2 = ((PlotBlock) node.getDefaultValue());
-                        roadBlock = new ItemStack(pb2.id, 1, pb2.data);
-                        break;
-                    case "wall.filling":
-                        val pb3 = ((PlotBlock) node.getDefaultValue());
-                        wallFilling = new ItemStack(pb3.id, 1, pb3.data);
-                        break;
-                    case "wall.height":
-                        wallHeight = (int) node.getDefaultValue();
-                        break;
-                    case "plot.bedrock":
-                        bedrock = (boolean) node.getDefaultValue();
-                        break;
-                }
-            }
+            super("Test", WorldType.NORMAL);
         }
 
         @Override
-        public void generate(String world) {
-            // Set values
-            for (ConfigurationNode node : getSetupObj().step) {
-                switch (node.getConstant()) {
-                    case "plot.height":
-                        node.setValue(String.valueOf(plotHeight));
-                        break;
-                    case "plot.size":
-                        node.setValue(String.valueOf(plotSize));
-                        break;
-                    case "plot.filling":
-                        node.setValue(plotFilling.getType().getId() + ":" + plotFilling.getDurability());
-                        break;
-                    case "plot.floor":
-                        node.setValue(plotFloor.getType().getId() + ":" + plotFloor.getDurability());
-                        break;
-                    case "wall.block":
-                        node.setValue(wall.getType().getId() + ":" + wall.getDurability());
-                        break;
-                    case "wall.block_claimed":
-                        node.setValue(wallClaimed.getType().getId() + ":" + wallClaimed.getDurability());
-                        break;
-                    case "road.width":
-                        node.setValue(String.valueOf(roadWidth));
-                        break;
-                    case "road.height":
-                        node.setValue(String.valueOf(roadHeight));
-                        break;
-                    case "road.block":
-                        node.setValue(roadBlock.getType().getId() + ":" + roadBlock.getDurability());
-                        break;
-                    case "wall.filling":
-                        node.setValue(wallFilling.getType().getId() + ":" + wallFilling.getDurability());
-                        break;
-                    case "wall.height":
-                        node.setValue(String.valueOf(wallHeight));
-                        break;
-                    case "plot.bedrock":
-                        node.setValue(String.valueOf(bedrock));
-                        break;
-                }
-            }
-            // Generate world
-            super.generate(world);
+        public void setup(String worldName) {
+            configureNode("plot.height", String.valueOf(plotHeight));
+            configureNode("plot.size", String.valueOf(plotWidth));
+            configureNode("plot.filling", plotBlock.getKey().getNamespace() + ":" + plotBlock.getKey().getKey());
+            configureNode("wall.place_top_block", String.valueOf(blockOnTopOfPlotBlock));
+            configureNode("plot.floor", plotFloorBlock.getKey().getNamespace() + ":" + plotFloorBlock.getKey().getKey());
+            configureNode("wall.block", topWallBlockUnClaimed.getKey().getNamespace() + ":" + topWallBlockUnClaimed.getKey().getKey());
+            configureNode("wall.block_claimed", topWallBlockClaimed.getKey().getNamespace() + ":" + topWallBlockClaimed.getKey().getKey());
+            configureNode("road.width", String.valueOf(roadWidth));
+            configureNode("road.height", String.valueOf(roadHeight));
+            configureNode("road.block", roadBlock.getKey().getNamespace() + ":" + roadBlock.getKey().getKey());
+            configureNode("wall.filling", wallFillingBlock.getKey().getNamespace() + ":" + wallFillingBlock.getKey().getKey());
+            configureNode("wall.height", String.valueOf(wallHeight));
+            configureNode("plot.bedrock", String.valueOf(hasBedrock));
+
+            super.setup(worldName);
         }
     }
 }
